177d14cbb597c406c1c47564f40e8ccd31375506
YRAhire2412 <yashahire10224@gmail.com>
2025-02-12 16:05:20 +0530

    Update EmailService.java

--- Agrosphere/Backend1/Backend/AgroSphere/src/main/java/com/app/service/EmailService.java
+++ Agrosphere/Backend1/Backend/AgroSphere/src/main/java/com/app/service/EmailService.java
@@ -8,25 +8,6 @@ import lombok.Value;
 
 @Service
 public class EmailService {
-//	 private final JavaMailSender mailSender;
-//
-//	    // Constructor injection for JavaMailSender
-//	    @Autowired
-//	    public EmailService(JavaMailSender mailSender) {
-//	        this.mailSender = mailSender;
-//	    }
-//
-//	    // Method to send registration success email
-//	    public void sendRegistrationEmail(String toEmail, String firstName) {
-//	        SimpleMailMessage message = new SimpleMailMessage();
-//	        message.setFrom("your-email@gmail.com");  // Sender's email address
-//	        message.setTo(toEmail);                   // Recipient's email address
-//	        message.setSubject("Registration Successful");
-//	        message.setText("Dear " + firstName + ",\n\nThank you for registering with us! Your account has been successfully created.\n\nBest regards,\nYour Company");
-//
-//	        // Send the email
-//	        mailSender.send(message);
-//	    }
 @org.springframework.beans.factory.annotation.Value("${spring.mail.username}")  // This will get the email set in application.properties
 	    private String fromEmail;
 
